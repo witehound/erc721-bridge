aa4f03e0648d63612e749a753cb2e2241e3b9a2b
witehound <netvid00@gmail.com>
2023-03-19 22:20:25 +0100

    set up file for bridge contract

--- contracts/contracts/Bridge.sol
+++ contracts/contracts/Bridge.sol
@@ -1,4 +1,17 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract Bridge {}
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Bridge is ReentrancyGuard, Ownable, IERC721Receiver {
+    uint256 costNative = 1 ether;
+    uint256 costCustom = 0.000075 ether;
+
+    struct Custody {
+        uint256 tokenId;
+        address holder;
+    }
+}
