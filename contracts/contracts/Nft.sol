94ae10edd76560925c1c4cbcb6a1cf95d0c196a1
witehound <netvid00@gmail.com>
2023-03-21 13:19:40 +0100

    set up filr for bridge nft contarct

---
+++ contracts/contracts/Nft.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract NFT is Ownable, ERC721Enumerable {
+    using Strings for uint256;
+    string public baseUri;
+    string public baseExtension = ".json";
+    uint256 public maxSupply = 1000;
+    bool public paused = false;
+
+    constructor() ERC721("Test Bridge", "BDG") {}
+}
