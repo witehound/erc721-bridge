410ac95350877608d38cb1e86295f9cf93f9ce84
witehound <netvid00@gmail.com>
2023-03-21 13:12:22 +0100

    updated the contract name

--- contracts/contracts/Bridge.sol
+++ contracts/contracts/Bridge.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract BridgeCustody is ReentrancyGuard, Ownable, IERC721Receiver {
+contract Vault is ReentrancyGuard, Ownable, IERC721Receiver {
     uint256 costNative = 1 ether;
     uint256 costCustom = 0.000075 ether;
 
