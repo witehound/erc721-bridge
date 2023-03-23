ac2639a93e924382b0862b3caa5ae3662826c0de
pythonguru101 <Thobias@crebos.online>
2023-03-23 23:24:26 +0100

    5 hours fixing rembrand

--- contracts/contracts/Nft.sol
+++ contracts/contracts/Nft.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
-
+//5 hours fixing rembrand
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
